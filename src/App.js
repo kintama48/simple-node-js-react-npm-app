cb89bcb5ac19274953a32b9d9a31566f891840b0
Ahmad Saad <64619851+saadpocalypse@users.noreply.github.com>
2022-08-31 17:12:36 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,11 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to SQA Final Project!</h1>
+          <h2>This is a dummy application to test Jenkins!</h2>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
